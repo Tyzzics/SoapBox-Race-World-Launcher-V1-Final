5cad01ade5d8a34614ec4ce1dba325e25ef2c824
Diogo Pedroso <Diogo Pedroso@DESKTOP-I5UV32Q>
2017-04-25 19:34:14 +0100

    Add project files.

---
+++ SoapBox Race World Launcher/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.passtxt = New System.Windows.Forms.TextBox()
+        Me.emailtxt = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txt_logingood = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.btn_login = New System.Windows.Forms.Button()
+        Me.ipv4server = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button1.CausesValidation = False
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(29, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(144, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(522, -2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(34, 29)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "X"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'passtxt
+        '
+        Me.passtxt.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.passtxt.Font = New System.Drawing.Font("Candara", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.passtxt.ForeColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(139, Byte), Integer), CType(CType(199, Byte), Integer))
+        Me.passtxt.Location = New System.Drawing.Point(305, 232)
+        Me.passtxt.Name = "passtxt"
+        Me.passtxt.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.passtxt.Size = New System.Drawing.Size(190, 15)
+        Me.passtxt.TabIndex = 4
+        Me.passtxt.WordWrap = False
+        '
+        'emailtxt
+        '
+        Me.emailtxt.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.emailtxt.Font = New System.Drawing.Font("Candara", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.emailtxt.ForeColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(139, Byte), Integer), CType(CType(199, Byte), Integer))
+        Me.emailtxt.Location = New System.Drawing.Point(324, 145)
+        Me.emailtxt.Name = "emailtxt"
+        Me.emailtxt.Size = New System.Drawing.Size(190, 15)
+        Me.emailtxt.TabIndex = 4
+        Me.emailtxt.WordWrap = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Candara", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(321, 124)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 18)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "E-mail address:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Candara", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(302, 213)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 18)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Password:"
+        '
+        'txt_logingood
+        '
+        Me.txt_logingood.BackColor = System.Drawing.Color.Transparent
+        Me.txt_logingood.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_logingood.ForeColor = System.Drawing.Color.White
+        Me.txt_logingood.Location = New System.Drawing.Point(12, 92)
+        Me.txt_logingood.Name = "txt_logingood"
+        Me.txt_logingood.Size = New System.Drawing.Size(527, 36)
+        Me.txt_logingood.TabIndex = 6
+        Me.txt_logingood.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.BackColor = System.Drawing.Color.Transparent
+        Me.CheckBox1.Font = New System.Drawing.Font("Candara", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.ForeColor = System.Drawing.Color.White
+        Me.CheckBox1.Location = New System.Drawing.Point(315, 166)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(109, 17)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "Remember E-mail"
+        Me.CheckBox1.UseVisualStyleBackColor = False
+        '
+        'btn_login
+        '
+        Me.btn_login.BackColor = System.Drawing.Color.Transparent
+        Me.btn_login.BackgroundImage = Global.SoapBox_Race_World_Launcher.My.Resources.Resources.botao2
+        Me.btn_login.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn_login.FlatAppearance.BorderSize = 0
+        Me.btn_login.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.btn_login.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_login.Font = New System.Drawing.Font("Candara", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_login.ForeColor = System.Drawing.Color.White
+        Me.btn_login.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.btn_login.Location = New System.Drawing.Point(288, 262)
+        Me.btn_login.Name = "btn_login"
+        Me.btn_login.Size = New System.Drawing.Size(96, 22)
+        Me.btn_login.TabIndex = 3
+        Me.btn_login.Text = "LOG IN"
+        Me.btn_login.UseVisualStyleBackColor = False
+        '
+        'ipv4server
+        '
+        Me.ipv4server.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.ipv4server.Font = New System.Drawing.Font("Candara", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ipv4server.ForeColor = System.Drawing.Color.FromArgb(CType(CType(29, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(144, Byte), Integer))
+        Me.ipv4server.Location = New System.Drawing.Point(33, 166)
+        Me.ipv4server.Name = "ipv4server"
+        Me.ipv4server.Size = New System.Drawing.Size(190, 15)
+        Me.ipv4server.TabIndex = 8
+        Me.ipv4server.WordWrap = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Candara", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(30, 145)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(139, 18)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Ipv4 Server Address:"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.BackgroundImage = Global.SoapBox_Race_World_Launcher.My.Resources.Resources.botao1
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Candara", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(397, 262)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 22)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "GAME PATH"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImage = Global.SoapBox_Race_World_Launcher.My.Resources.Resources.launch
+        Me.ClientSize = New System.Drawing.Size(551, 299)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ipv4server)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.txt_logingood)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.passtxt)
+        Me.Controls.Add(Me.emailtxt)
+        Me.Controls.Add(Me.btn_login)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents passtxt As TextBox
+    Friend WithEvents emailtxt As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txt_logingood As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents ipv4server As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents btn_login As Button
+End Class
---
+++ SoapBox Race World Launcher/Form1.vb
@@ -0,0 +1,180 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.Net
+Imports System.Security.Cryptography
+Imports System.Text
+Imports System.Xml
+Imports System.Xml.Serialization
+Imports System.Xml.Linq
+Imports Microsoft.VisualBasic
+Public Class Form1
+    Dim token As Integer
+    Dim arguments As String
+    Dim userid As Integer
+    Dim gamepath As String
+    Dim drag As Boolean
+    Dim mousex As Integer
+    Dim mousey As Integer
+    Dim path As String = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+    Dim filepath As String = path & "\SoapBoxLauncher\email.txt"
+    Dim ipremember As String = path & "\SoapBoxLauncher\ip.txt"
+    Dim filepathgame As String = path & "\SoapBoxLauncher\gamepath.txt"
+    Dim normalpassword As String
+    Dim criptedpassword As String
+    Dim fullxml As String
+    Dim UserId_value As Integer
+    Dim Token_value As Integer
+    'making the window movible from anywhere
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDown
+        drag = True
+        mousex = Windows.Forms.Cursor.Position.X - Me.Left
+        mousey = Windows.Forms.Cursor.Position.Y - Me.Top
+    End Sub
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
+        If drag Then
+            Me.Top = Windows.Forms.Cursor.Position.Y - mousey
+            Me.Left = Windows.Forms.Cursor.Position.X - mousex
+        End If
+    End Sub
+    Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseUp
+        drag = False
+    End Sub
+    'magic beggins
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'creates the soapbox folder
+        My.Computer.FileSystem.CreateDirectory(path & "\SoapBoxLauncher")
+        'email remember
+        If Not System.IO.File.Exists(filepath) Then
+            System.IO.File.Create(filepath).Dispose()
+        End If
+        emailtxt.Text = System.IO.File.ReadAllText(filepath)
+        'gamepath remember
+        If Not System.IO.File.Exists(filepathgame) Then
+            System.IO.File.Create(filepathgame).Dispose()
+        End If
+        gamepath = System.IO.File.ReadAllText(filepathgame)
+        'ipv4 remember
+        If Not System.IO.File.Exists(ipremember) Then
+            System.IO.File.Create(ipremember).Dispose()
+        End If
+        ipv4server.Text = System.IO.File.ReadAllText(ipremember)
+    End Sub
+    'X button
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+    End Sub
+    'pass encription
+    Public Function passwordEncryptSHA(ByVal normalpassword As String) As String
+        Dim sha As New SHA1CryptoServiceProvider
+        Dim bytesToHash() As Byte
+        bytesToHash = System.Text.Encoding.ASCII.GetBytes(normalpassword)
+        bytesToHash = sha.ComputeHash(bytesToHash)
+        Dim encPassword As String = ""
+        For Each b As Byte In bytesToHash
+            encPassword += b.ToString("x2")
+        Next
+        Return encPassword
+    End Function
+    Public Function GetUrl(ByVal PostData As String) As String
+        Try
+            Dim Http2 As HttpWebRequest = WebRequest.Create("Http://" & ipv4server.Text & "/soapbox/Engine.svc/user/authenticateUser?email=" & emailtxt.Text & "&password=" & criptedpassword)
+            Http2.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate")
+            If Not String.IsNullOrEmpty(PostData) Then
+                Http2.Method = "POST"
+                Dim lbPostBuffer As Byte() = Encoding.Default.GetBytes(PostData)
+                Http2.ContentLength = lbPostBuffer.Length
+                Using PostStream As Stream = Http2.GetRequestStream()
+                    PostStream.Write(lbPostBuffer, 0, lbPostBuffer.Length)
+                End Using
+            End If
+            Using WebResponse As HttpWebResponse = Http2.GetResponse()
+                Dim responseStream As Stream = WebResponse.GetResponseStream()
+                If (WebResponse.ContentEncoding.ToLower().Contains("gzip")) Then
+                    responseStream = New GZipStream(responseStream, CompressionMode.Decompress)
+                ElseIf (WebResponse.ContentEncoding.ToLower().Contains("deflate")) Then
+                    responseStream = New DeflateStream(responseStream, CompressionMode.Decompress)
+                End If
+                Dim reader As StreamReader = New StreamReader(responseStream, Encoding.Default)
+                Dim html As String = reader.ReadToEnd()
+                responseStream.Close()
+                Return html
+            End Using
+        Catch
+        End Try
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btn_login.Click
+        txt_logingood.Text = ""
+        If gamepath = "" Then
+            txt_logingood.Text = "Select the game folder first."
+        Else
+            'this checks if the textbox has an "@" and a "." like a normal Email does.
+            If emailtxt.Text.Contains("@") And emailtxt.Text.Contains(".") And passtxt.TextLength <> 0 And ipv4server.TextLength <> 0 Then
+                If CheckBox1.Checked = True Then
+                    'CREATES A FILE THAT WILL SAVE THE EMAIL
+                    If Not System.IO.File.Exists(filepath) Then
+                        System.IO.File.Create(filepath).Dispose()
+                        System.IO.File.WriteAllText(filepath, emailtxt.Text)
+                    Else
+
+                        System.IO.File.Delete(filepath)
+                        System.IO.File.Create(filepath).Dispose()
+                        System.IO.File.WriteAllText(filepath, emailtxt.Text)
+                    End If
+                End If
+                normalpassword = passtxt.Text
+                Call passwordEncryptSHA(normalpassword)
+                criptedpassword = passwordEncryptSHA(normalpassword)
+                fullxml = GetUrl(String.Empty)
+                If Not fullxml = "" Then
+                    Dim element As XDocument = XDocument.Parse(fullxml)
+                    Dim UserId = From d In element.Descendants("LoginData")
+                                 Select d.Element("UserId").Value
+                    Dim UserId_value As Integer = UserId.First()
+                    Dim Token = From d In element.Descendants("LoginData")
+                                Select d.Element("LoginToken").Value
+                    Dim Token_value As String = Token.First()
+                    arguments = " US " & "http://" & ipv4server.Text & "/soapbox/Engine.svc " & Token_value & " " & UserId_value
+                    Try
+                        Process.Start(gamepath & "\nfsw.exe", arguments)
+                        txt_logingood.Text = "Loged in successfully, Launching the game."
+                        If Not System.IO.File.Exists(ipremember) Then
+                            System.IO.File.Create(ipremember).Dispose()
+                            System.IO.File.WriteAllText(ipremember, ipv4server.Text)
+                        Else
+                            System.IO.File.Delete(ipremember)
+                            System.IO.File.Create(ipremember).Dispose()
+                            System.IO.File.WriteAllText(ipremember, ipv4server.Text)
+                        End If
+                        GoTo godgamepath
+                    Catch ex As System.ComponentModel.Win32Exception
+                        GoTo badgamepath
+                    End Try
+                Else
+                    txt_logingood.Text = "Ip, email or password its not correctly whriten."
+                    GoTo godgamepath
+                End If
+            Else
+                txt_logingood.Text = "Email or password its not correctly whriten."
+                GoTo godgamepath
+            End If
+badgamepath:
+            txt_logingood.Text = "Game Path is not correct, Ex: C:\Program Files (x86)\NFS World" & ControlChars.NewLine & "The folder should contain the nfsw.exe"
+        End If
+godgamepath:
+    End Sub
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        txt_logingood.Text = ""
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            gamepath = FolderBrowserDialog1.SelectedPath
+            If Not System.IO.File.Exists(filepathgame) Then
+                System.IO.File.Create(filepathgame).Dispose()
+                System.IO.File.WriteAllText(filepathgame, gamepath)
+            Else
+                System.IO.File.Delete(filepathgame)
+                System.IO.File.Create(filepathgame).Dispose()
+                System.IO.File.WriteAllText(filepathgame, gamepath)
+            End If
+        End If
+    End Sub
+End Class
